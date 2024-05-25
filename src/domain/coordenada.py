92684e35d7c14b15c6391e6d6f441d2b4b4c3095
Pol Rubio Borrego <prubio@edu.tecnocampus.cat>
2024-05-25 13:36:45 +0200

    `Coordenada.distancia()` implemented

--- src/domain/coordenada.py
+++ src/domain/coordenada.py
@@ -1,4 +1,9 @@
+from geopy.distance import geodesic
+
 class Coordenada:
     def __init__(self, latitud: float, longitud: float) -> None:
         self.latitud: float = latitud
         self.longitud: float = longitud
+    
+    def distancia(self, altre: "Coordenada") -> float:
+        return geodesic((self.latitud, self.longitud), (altre.latitud, altre.longitud)).meters
